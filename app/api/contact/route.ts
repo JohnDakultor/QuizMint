d17920e6f414f1dbffa3019fa6e5c58103a86335
Germel Marata <germeljohnmarata@gmail.com>
2025-12-20 14:43:42 +0300

    changed domain for email in contact api

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: Request) {
     }
 
     await resend.emails.send({
-      from: "QuizMint Contact <support@quizmint.com>",
+      from: "QuizMint Contact <support@quizmintai.com>",
       to: [process.env.CONTACT_RECEIVER_EMAIL!],
       replyTo: email,
       subject: `New Contact Message from ${name}`,
