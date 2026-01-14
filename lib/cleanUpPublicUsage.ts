315fccce1ccf0dbdd691c9d8ae77baed4b90e83e
Germel Marata <germeljohnmarata@gmail.com>
2026-01-14 12:45:32 +0300

    changed cron time to two weeks

--- lib/cleanUpPublicUsage.ts
+++ lib/cleanUpPublicUsage.ts
@@ -2,7 +2,7 @@
 import { prisma } from "@/lib/prisma";
 
 export async function cleanupOldPublicUsage() {
-  const TWO_WEEKS_AGO = new Date(Date.now() - 3 * 60 * 60 * 1000); //new Date(Date.now() - 14 * 24 * 60 * 60 * 1000);
+  const TWO_WEEKS_AGO = new Date(Date.now() - 14 * 24 * 60 * 60 * 1000);
 
   const deleted = await prisma.publicUsage.deleteMany({
     where: {
