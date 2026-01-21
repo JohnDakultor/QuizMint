6839824387cc1469fc2c1b8d9af338acbd0bcd37
Germel Marata <germeljohnmarata@gmail.com>
2026-01-21 23:38:59 +0300

    fixed policAcceptance api

--- app/api/policyAcceptance/route.ts
+++ app/api/policyAcceptance/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const userId = Number(body.userId); // ✅ FIX HERE
+    const userId = (body.userId); // ✅ FIX HERE
     const policyType = body.policyType;
 
     if (!userId || !policyType) {
